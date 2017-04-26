8b91149b9b1ccf26fb0583012ce72656755aa9ab
shadowblade7536 <shadowblade7536@gmail.com>
2017-04-26 07:01:05 +0400

    Working on porting to client side only now

--- web/js/index.js
+++ web/js/index.js
@@ -1,3 +1,86 @@
-var socket = io.connect({
-    secure: false
-  }) //TODO: Change this to true after SSL
+function EmailRecon(email) {
+	var psbdmp;
+	var haveibeenpwned_breach;
+	var haveibeenpwned_paste;
+	var hacked_db;
+	var hacked_email;
+
+	request('http://psbdmp.com/api/search/email/' + email, function(err, res, body) {
+		if (err) {
+			log.error(err);
+		}
+		if (res.statusCode == 404) {
+			psbdmp = false
+		} else {
+			psbdmp = body
+		}
+		PSBDMPResults(psbdmp)
+
+	})
+
+	var options = {
+		url: 'https://haveibeenpwned.com/api/v2/breachedaccount/' + email,
+		headers: {
+			'User-Agent': 'StalkerJS - A NodeJS WebApp for User Recon'
+		}
+	}
+
+	request(options, function(err, res, body) {
+		if (err) {
+			log.error(err);
+		}
+		if (res.statusCode == 404) {
+			haveibeenpwned_breach = false
+		} else {
+			haveibeenpwned_breach = body
+		}
+		HaveIBeenPwnedBreachResults(haveibeenpwned_breach)
+	})
+
+	sleep.sleep(1)
+
+	var options = {
+		url: 'https://haveibeenpwned.com/api/v2/pasteaccount/' + email,
+		headers: {
+			'User-Agent': 'StalkerJS - A NodeJS WebApp for User Recon'
+		}
+	}
+
+	request(options, function(err, res, body) {
+		if (err) {
+			log.error(err);
+		}
+		if (res.statusCode == 404) {
+			haveibeenpwned_paste = false
+		} else {
+			haveibeenpwned_paste = body
+		}
+		HaveIBeenPwnedPasteResults(haveibeenpwned_paste)
+
+	})
+
+	request('https://www.hacked-db.com/api/v1/email/' + email, function(err, res, body) {
+		if (err) {
+			log.error(err);
+		}
+		if (res.statusCode == 404) {
+			hacked_db = false
+		} else {
+			hacked_db = body
+		}
+		HackedDBResults(hacked_db)
+	})
+
+	request('https://www.hacked-emails.com/api?q=' + email, function(err, res, body) {
+		if (err) {
+			log.error(err);
+		}
+		if (res.statusCode == 404) {
+			hacked_emails = false
+		} else {
+			hacked_emails = body
+		}
+		HackedEmailResults(hacked_emails)
+	})
+	DoneResults()
+}
